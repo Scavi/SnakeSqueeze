cde59d73dec44ed7f096ef8791209d1ede17f281
heymel <heymel@amazon.de>
2018-12-03 23:57:54 +0100

    Added compatibilty to Python 3.4

--- src/snake_squeeze/Util/FileHelper.py
+++ src/snake_squeeze/Util/FileHelper.py
@@ -1,18 +1,15 @@
-from pathlib import Path
-
-
 class FileHelper(object):
 
     # Reads the file to the given file path and returns all lines in string array
     @staticmethod
-    def read_file(file_path: Path):
+    def read_file(file_path):
         result = []
 
         if file_path.exists() is False:
             return result
 
         try:
-            with open(file_path) as fp:
+            with open(str(file_path)) as fp:
                 for line in iter(fp.readline, ''):
                     result.append(line)
         finally:
