1caa9720046b6f9f11c5114933419505b9d4c359
heymel <heymel@amazon.de>
2021-12-02 01:41:29 +0100

    Solved AoC Day 1

--- src/snake_squeeze/Y2021/SonarSweep.py
+++ src/snake_squeeze/Y2021/SonarSweep.py
@@ -7,7 +7,7 @@ class SonarSweep:
         return len([i for i in range(size, len(data)) if
                     sum(data[i - size: i]) > sum(data[i - size - 1: i - 1])])
 
-    # Note: numpy solution takes for this example 3-4x as long as the solution above
+    # Note: numpy solution
     # @staticmethod
     # def solve(data: List[int], size: int) -> int:
     #     slices = sliding_window_view(np.array(data), size)
